219be5fed6b6d9a97abfda5680bc558ad5271559
Adrian Salgado <162409814+asalgado27@users.noreply.github.com>
2024-04-04 23:31:18 -0400

    Add files via upload

---
+++ World.java
@@ -0,0 +1,11 @@
+public class World{
+    int height;
+    int width;
+    int pointsLeft = 0;
+    int pointsRight = 0;
+ 
+    public World(int initWidth, int initHeight){
+        width = initWidth;
+        height = initHeight;
+    }
+}
