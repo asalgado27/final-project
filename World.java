3003f12388993e53c197856675a6197d2c84496d
aliciaxin <165958406+aliciaxin@users.noreply.github.com>
2024-04-06 15:59:54 -0400

    Updated World.java with person updating methods

--- World.java
+++ World.java
@@ -3,9 +3,18 @@ public class World{
     int width;
     int pointsLeft = 0;
     int pointsRight = 0;
- 
+    Person person = new Person();
+    
     public World(int initWidth, int initHeight){
         width = initWidth;
         height = initHeight;
+    }
+
+    public void drawPerson(Graphics g){
+        person.draw(g);
+    }
+
+    public void updatePerson(double time){
+        person.update(this, time);
     }
 }
