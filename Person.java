6a7072eba2769833833196b29fd67bdefa281fb8
aliciaxin <165958406+aliciaxin@users.noreply.github.com>
2024-04-08 21:02:32 -0400

    walking animation update: untested but should? work

--- Person.java
+++ Person.java
@@ -17,16 +17,28 @@ class Person{
     Pair velocity;
     Pair acceleration;
     double radius;
+    int animationCounter;
+    boolean horizontalMotion;
     private BufferedImage avatar = null;
+    private BufferedImage walk1 = null;
+    private BufferedImage walk2 = null;
+    private BufferedImage walk3 = null;
+    private BufferedImage walk4 = null;
 
     public Person() {
         position = new Pair(262.0, 384.0);
         velocity = new Pair((double)(0), (double)(0));
         acceleration = new Pair(0,300);
         radius = 5;
+        horizontalMotion = false;
+        animationCounter = 0;
         
         try{
             avatar = ImageIO.read(Main.class.getResource("avatar.png"));
+            walk1 = ImageIO.read(Main.class.getResource("walk1.png"));
+            walk2 = ImageIO.read(Main.class.getResource("walk2.png"));
+            walk3 = ImageIO.read(Main.class.getResource("walk3.png"));
+            walk4 = ImageIO.read(Main.class.getResource("walk4.png"));
         } catch (IOException e){
             System.err.println("IOException");
             System.exit(1);
@@ -45,10 +57,14 @@ class Person{
         if (position.x <=-10){
             this.setVelocityX(0);
             this.setPosition(new Pair(-9, position.y));
+            horizontalMotion = false;
+            animationCounter = 0;
         }
         if (position.x>= 1024-100){
             this.setVelocityX(0);
             this.setPosition(new Pair(1024-100-1, position.y));
+            horizontalMotion = false;
+            animationCounter = 0;
         }
     }
 
@@ -69,8 +85,26 @@ class Person{
     }
     
     public void draw(Graphics g){
-        //FROG
-        g.drawImage(avatar,  (int)position.x, (int)position.y, null);
+        if (!horizontalMotion){
+            g.drawImage(avatar,  (int)position.x, (int)position.y, null);
+        }
+        if (horizontalMotion){
+            animationCounter++;
+            //divided just stores an integer that tells the animation to change which one is being drawn every 10 frames
+            int divided = animationCounter/10%4;
+            if (divided==0){
+                g.drawImage(walk1,  (int)position.x, (int)position.y, null);
+            }
+            if (divided==1){
+                g.drawImage(walk2,  (int)position.x, (int)position.y, null);
+            }
+            if (divided==2){
+                g.drawImage(walk3,  (int)position.x, (int)position.y, null);
+            }
+            if (divided==3){
+                g.drawImage(walk4,  (int)position.x, (int)position.y, null);
+            }
+        }
     }
 
     
@@ -79,9 +113,11 @@ class Person{
         
         if (c == 'a'){
             this.setVelocityX(-200);
+            horizontalMotion = true;
         }
         if (c == 'd'){
             this.setVelocityX(200);
+            horizontalMotion = true;
         }
         if (c == 's'){
             this.setVelocityY(200);
@@ -95,9 +131,13 @@ class Person{
     public void antimovement(char c){
         if (c == 'a' || c=='d'){
             this.setVelocityX(0);
+            horizontalMotion = false;
+            animationCounter = 0;
         }
         if (c == 's' || c == 'w'){
             this.setVelocityY(0);
+            horizontalMotion = false;
+            animationCounter = 0;
         }
     }
 
