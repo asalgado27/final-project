d6a8b402f079268b9f55b2db46534ceee50151fe
Adrian Salgado <162409814+asalgado27@users.noreply.github.com>
2024-04-14 18:52:13 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -13,8 +13,48 @@ public class Main extends JPanel implements KeyListener{
     public static final int HEIGHT = 768;
     public static final int FPS = 60;
 
+    public static final int lavaWidth = 1200;
+    public static final int lavaHeight = 768;
+
     public char c = '0';
     World homebase;
+    World lavaBiome;
+
+    World currentWorld;
+
+    public Main(){
+        homebase = new World(this, WIDTH, HEIGHT);
+        homebase.createHomebase();
+
+        lavaBiome = new World(this, lavaWidth, lavaHeight);
+        lavaBiome.createLavaBiome(homebase.person);
+
+        // Establish homebase as the current world
+        this.currentWorld = homebase;
+
+        this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        this.addKeyListener(this); 
+        this.setFocusable(true); 
+        this.requestFocus(); 
+        Thread mainThread = new Thread(new Runner());
+        mainThread.start();
+    }
+
+    public static void main(String[] args){
+        JFrame frame = new JFrame("Homebase");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Main mainInstance = new Main();
+        frame.setContentPane(mainInstance);
+        frame.pack();
+        frame.setVisible(true);
+    }
+ 
+ 
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        currentWorld.draw(g);
+        currentWorld.drawPerson(g);
+    }
 
  
     class Runner implements Runnable{
@@ -52,30 +92,19 @@ public class Main extends JPanel implements KeyListener{
         super.addNotify();
         requestFocus();
     }
- 
-    public Main(){
-        homebase = new World(WIDTH, HEIGHT);
-        this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
-        this.addKeyListener(this); 
-        this.setFocusable(true); 
-        this.requestFocus(); 
-        Thread mainThread = new Thread(new Runner());
-        mainThread.start();
+
+    // Open the door the person is at
+    public void openDoor() {
+        // Check if person is near door
+        if (homebase.person.position.x > homebase.doors[0].position.x && homebase.person.position.x < homebase.doors[0].position.x + homebase.doors[0].dimensions.x) {
+            // Move person to new world
+            this.currentWorld = lavaBiome;
+            dimLava();
+        }
     }
-     
-    public static void main(String[] args){
-        JFrame frame = new JFrame("GAME ON");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Main mainInstance = new Main();
-        frame.setContentPane(mainInstance);
-        frame.pack();
-        frame.setVisible(true);
-    }
- 
- 
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        homebase.draw(g);
-        homebase.drawPerson(g);
+
+    // Change dimensions to those of Lava Biome
+    public void dimLava() {
+        this.setPreferredSize(new Dimension(lavaWidth, lavaHeight));
     }
 }
