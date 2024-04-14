3128927af81d4d6a851db7ec6486d86c688efd27
Adrian Salgado <162409814+asalgado27@users.noreply.github.com>
2024-04-14 18:02:24 -0400

    Fixed errors

--- Item.java
+++ Item.java
@@ -1,21 +1,21 @@
 // basic structure. Will work out the details when we need to create objects.
 
-/*
+import java.awt.Color;
 import java.awt.Graphics;
 
-public abstract class Item() {
-	Pair position = new Pair();
-	Color color = new Color();
+public abstract class Item {
+	Pair position;
+	Color color;
 
 	public Item() {}
 
-	public abstract void draw(Graphics g) {}
+	public void draw(Graphics g) {}
 
 	public void collect() {}
-		// item disappears (or pops, or does some motion) when the person collects it
+	// item disappears (or pops, or does some motion) when the person collects it
 
-	public void use() {} 
-		// unleash powers of the item
-		// remove from inventory if item is inventory; or, if item is immediate-use, immediately use
+	public Color use() {return null;} 
+	// unleash powers of the item
+	// remove from inventory if item is inventory; or, if item is immediate-use, immediately use
 }
-*/
+
