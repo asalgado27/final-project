31de5468eb6380e053bccfdbced0c77fe194db21
aliciaxin <165958406+aliciaxin@users.noreply.github.com>
2024-04-15 21:21:24 -0400

    Update Item.java, inventory added

--- Item.java
+++ Item.java
@@ -6,16 +6,30 @@ import java.awt.Graphics;
 public abstract class Item {
 	Pair position;
 	Color color;
+    Main m;
+    Person p;
+    boolean visible;
 
-	public Item() {}
+	public Item(Main m, Person p) {
+        this.m = m;
+        this.p = p;
+        m.uncollectedItems.add(this);
+        visible = true;
+    }
 
 	public void draw(Graphics g) {}
 
-	public void collect() {}
+	public void collect() {
+        p.inventory.add(this);
+        m.uncollectedItems.remove(this);
+        visible = false;
+    }
 	// item disappears (or pops, or does some motion) when the person collects it
 
-	public void use() {} 
+	public void use() {
+        p.inventory.remove(this);
+        
+    } 
 	// unleash powers of the item
 	// remove from inventory if item is inventory; or, if item is immediate-use, immediately use
 }
-
