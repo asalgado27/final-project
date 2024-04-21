b63b62432a002d3dbd167c36a1aa91a5cb492e4c
Adrian Salgado <162409814+asalgado27@users.noreply.github.com>
2024-04-21 00:20:21 -0400

    Update Door.java

--- Door.java
+++ Door.java
@@ -2,12 +2,11 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 class Door {
-    // Fields indicating location and size of the platform
     Pair position;
     static Pair dimensions = new Pair(75, 110);
 
-    public Door(int xPos, int yPos) {
-        position = new Pair(xPos, yPos);
+    public Door(Pair position) {
+        this.position = position;
     }
 
     // Draws the platform
