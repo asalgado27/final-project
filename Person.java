c5d31654b26bc7a2e966cd889f861a8bfeeade73
Adrian Salgado <162409814+asalgado27@users.noreply.github.com>
2024-04-21 20:20:51 -0400

    Update Person.java

--- Person.java
+++ Person.java
@@ -30,7 +30,6 @@ class Person{
 
     Platform currentPlatform;
 
-    boolean onLadder = false;
     boolean canJump = true;
 
     Main main;
@@ -132,18 +131,14 @@ class Person{
         // Only check if on ladder if platforms are nearby
         if (currentWorld.platforms != null) {
             for (Platform p : currentWorld.platforms) {
-                if (position.x + dimensions.x / 2 < p.ladderPos + p.ladderWidth && position.x + dimensions.x / 2 > p.ladderPos) {
-                    if (this.position.y + this.dimensions.y > p.position.y && this.position.y + this.dimensions.y <= p.position.y + p.ladderLength) { // if within height of ladder
-                        canJump = true;
-                        onLadder = true;
-                        setAcceleration(0); //  climb with constant velocity
+                if (this.position.y > p.position.y && this.position.y + this.dimensions.y < p.position.y + p.ladderLength) {
+                    if (position.x + dimensions.x / 2 < p.ladderPos + p.ladderWidth && position.x + dimensions.x / 2 > p.ladderPos) {
+                        setAcceleration(0);
                         upwardVelocity = 80;
                         downwardVelocity = 80;
-                        System.out.println("We are on ladder");
                     }
                     else {
-                        onLadder = false;
-                        setAcceleration(250); // person responds to gravity
+                        setAcceleration(250);
                         upwardVelocity = 500;
                     }
                 }
@@ -155,21 +150,18 @@ class Person{
         // Only check if on platform if platforms are nearby
         if (currentWorld.platforms != null) {
             for (Platform p : currentWorld.platforms) {
-                if (position.y + dimensions.y > currentPlatform.position.y && p.position.y > currentPlatform.position.y) {
+                if (position.y > currentPlatform.position.y && p.position.y > currentPlatform.position.y) {
                     currentPlatform = p;
-                    System.out.println("current platform has y-coord " + currentPlatform.position.y);
                 }
                 else if (position.y + dimensions.y < p.position.y && p.position.y < currentPlatform.position.y) {
                     currentPlatform = p;
-                    System.out.println("current platform has y-coord " + currentPlatform.position.y);
-
                 }
                 if (position.y + dimensions.y >= currentPlatform.position.y) {
                     canJump = true;
                     setVelocityY(0);
                     this.setPosition(new Pair(position.x, currentPlatform.position.y - dimensions.y));
                 }
-                else if (onLadder == false) {
+                else {
                     canJump = false;
                 }
             }
@@ -246,7 +238,7 @@ class Person{
             this.setVelocityX(200);
             horizontalRMotion = true;
         }
-        if (c == 's' && onLadder == true){
+        if (c == 's'){
             this.setVelocityY(downwardVelocity);
             //don't change the velocity. can make the person duck
         }
