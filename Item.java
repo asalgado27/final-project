f285b9b0bf79e874ec62f8322c97944cadda71d5
Adrian Salgado <162409814+asalgado27@users.noreply.github.com>
2024-04-22 00:07:44 -0400

    Update Item.java

--- Item.java
+++ Item.java
@@ -4,29 +4,29 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public abstract class Item {
-	Pair position;
-	Color color;
+    Pair position;
+    Color color;
     Main m;
     Person p;
     boolean visible;
 
-	public Item(/*Main m, Person p*/) {
+    public Item(/*Main m, Person p*/) {
         // this.m = m;
         //this.p = p;
         // m.uncollectedItems.add(this);
         visible = true;
     }
 
-	public void draw(Graphics g) {}
+    public void draw(Graphics g) {}
 
-	public void checkAndCollect() {
+    public void checkAndCollect() {
         p.inventory.add(this);
         m.uncollectedItems.remove(this);
         visible = false;
     }
 	// item disappears (or pops, or does some motion) when the person collects it
 
-	public void use() {
+    public void use() {
         p.inventory.remove(this);
         
     } 
