b270abe47ba0e77ee62cb284009311bbfeb8eab3
aliciaxin <165958406+aliciaxin@users.noreply.github.com>
2024-04-27 16:11:13 -0400

    Update Door.java

--- Door.java
+++ Door.java
@@ -5,22 +5,40 @@ class Door {
     Pair position;
     static Pair dimensions = new Pair(75, 110);
     Color doorColor;
+    int neededKeys[];
 
     // Fields to keep track of world door is located in and world it leads to
     World currentWorld;
     public World nextWorld;
 
     // Default constructor
-    public Door(World currentWorld, World nextWorld, Pair position) {
+    public Door(World currentWorld, World nextWorld, Pair position, int neededKeys[]) {
         this.position = position;
         this.currentWorld = currentWorld;
         this.nextWorld = nextWorld;
+        this.neededKeys = neededKeys;
     }
 
     // Constructor if we want the door to have a special look
-    public Door(Color doorColor, World currentWorld, World nextWorld, Pair position) {
-        this(currentWorld, nextWorld, position);
+    public Door(Color doorColor, World currentWorld, World nextWorld, Pair position, int neededKeys[]) {
+        this(currentWorld, nextWorld, position, neededKeys);
         this.doorColor = doorColor;
+    }
+
+    //checks if the needed keys are in the inventory
+    public boolean canOpen(){
+        System.out.println(neededKeys);
+        if (neededKeys.length==0){
+            return true;
+        }
+        for (int i = 0; i<neededKeys.length-1; i++){
+            //if neededKeys are not contained in the inventory, return false
+            if (!currentWorld.person.keyInventory.contains(neededKeys[i])){
+                return false;
+            } 
+            System.out.println(currentWorld.person.keyInventory.contains(neededKeys[i]));
+        }
+        return true;
     }
 
     // Draws the platform
