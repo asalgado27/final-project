09acd3bf463a8aaef71a4417d9dc7f6144a8a7bf
aliciaxin <165958406+aliciaxin@users.noreply.github.com>
2024-05-05 15:00:21 -0400

    Update Door.java

--- Door.java
+++ Door.java
@@ -25,28 +25,20 @@ class Door {
         this.doorColor = doorColor;
     }
 
-    //checks if the needed keys are in the inventory
+    // Checks if the needed keys are in the inventory
     public boolean canOpen(){
 
-        // DELETE THIS RETURN STATEMENT!! This is only here so that we can move between worlds until the bug is figured out 
-        // @Alicia I commented out your print statements for now so that the code runs so you may have to uncomment them when you resume debugging
-        return true;
-
-        /*
-
-        System.out.println(neededKeys);
         if (neededKeys.length==0){
             return true;
         }
-        for (int i = 0; i<neededKeys.length-1; i++){
+        for (int i = 0; i<neededKeys.length; i++){
             //if neededKeys are not contained in the inventory, return false
             if (!currentWorld.person.keyInventory.contains(neededKeys[i])){
                 return false;
             } 
-            System.out.println(currentWorld.person.keyInventory.contains(neededKeys[i]));
         }
         return true;
-        */
+        
     }
 
     // Draws the platform
