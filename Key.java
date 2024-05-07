f4ea72fbbd06f53c8744c9ecdb67055499d985fe
Adrian Salgado <162409814+asalgado27@users.noreply.github.com>
2024-05-06 23:22:23 -0400

    Update Key.java

--- Key.java
+++ Key.java
@@ -5,12 +5,14 @@ import java.awt.Image;
 import javax.imageio.ImageIO;
 import java.io.IOException;
 
-public class Key extends Item {
+public class Key implements Item {
 	Pair position;
 	static int radius = 15;
 	Color color;
+	Main main;
 	Random rand;
 	int index;
+	boolean visible;
 
 	Platform platform;
 	Person person;
@@ -19,16 +21,17 @@ public class Key extends Item {
 	private Image keyImage = null;
 	
 	// Constructs a Key object
-	public Key(Platform platform, World world, int index) {
-		super(world.main, world.person);
+	public Key(Platform platform, Main main, World world, int index) {
 
         main.uncollectedItems.add(this);
 		this.platform = platform;
 		this.person = world.person;
+		this.main = main;
 		rand = new Random();
 		color = new Color(rand.nextFloat(), rand.nextFloat(), rand.nextFloat());
 		position = new Pair(platform.position.x + platform.dimensions.x / 2, platform.position.y - 52);
 		this.index = index;
+		this.visible = true;
 		try {
             keyImage = ImageIO.read(Main.class.getResource("key.png"));
             keyImage = keyImage.getScaledInstance((int)50,(int)50, 1);
@@ -48,7 +51,6 @@ public class Key extends Item {
 
 	// If the person comes into contact with the key, it will disappear and be stored in the person's keyInventory.
     public void checkAndCollect() {
-    	super.checkAndCollect();
 		boolean sameYvalue = (person.position.y + person.dimensions.y / 2 - position.y < 10) && (person.position.y + person.dimensions.y / 2 - position.y > -10);
     	boolean sameXvalue = (person.position.x + person.dimensions.x / 2 - position.x < 10 && person.position.x + person.dimensions.x / 2 - position.x > -10);
     	if (sameYvalue && sameXvalue) {
@@ -57,6 +59,13 @@ public class Key extends Item {
 			main.uncollectedItems.remove(this);
     	}
     }
+
+	// Item disappears (or pops, or does some motion) when the person collects it
+    public void use() {
+        if (this instanceof Key){
+            person.keyInventory.remove(this.index);
+        }
+    } 
 		
 
 }
