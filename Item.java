d9c6eeaad9a77df9fc06f0254d5dccc7f432a24b
Adrian Salgado <162409814+asalgado27@users.noreply.github.com>
2024-05-06 23:22:36 -0400

    Update Item.java

--- Item.java
+++ Item.java
@@ -5,33 +5,14 @@ import java.awt.Graphics;
 // This class was originally created because we wanted to have many types of Items, such as powerups as well as Keys.
 // Unfortunately, we only had time to fully implement Key, which is the sole child class of Item.
 
-public abstract class Item {
-    Pair position;
-    Color color;
-    Main main;
-    Person person;
-    boolean visible;
-
-    int index;
-
-    // Constructs the Item object
-    public Item(Main main, Person person) {
-        this.main = main;
-        this.person = person;
-        visible = true;
-    }
+public interface Item {
 
     // Requires that all classes that extends Item have a draw method
-    public void draw(Graphics g) {}
+    default void draw(Graphics g) {}
 
     // Requires that all classes that extends Item have a checkAndCollect method
-    public void checkAndCollect() {}
+    default void checkAndCollect() {}
 	
     // Item disappears (or pops, or does some motion) when the person collects it
-    public void use() {
-        if (this instanceof Key){
-            person.keyInventory.remove(this.index);
-        }
-        
-    } 
+    default void use() {} 
 }
